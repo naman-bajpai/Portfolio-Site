ba1f86f0debb183d5b4924e68b19e8370a4d8555
Naman Bajpai <namanbajpai4@gmail.com>
2024-02-26 23:30:26 -0500

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React, {useEffect,useState} from 'react';
 import { Link } from 'react-router-dom';
 
+import {style} from '../style';
+import {navLinks} from '../constants';
+import {logo, menu, close} from '../assets';
+
+
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    
+    <nav
+      className='`${styles.paddingX}`'
+    >
+      
+    </nav>
   ) 
 }
 
